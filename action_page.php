a4cdc783611eadac7fd9a9f846799fc546691f68
surjeetkum <surjeetembedded@gmail.com>
2023-05-30 13:14:02 +0530

    Update and rename index to action_page.php

---
+++ action_page.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html> 
+<html> 
+  
+<head>
+    <title>
+        HTML form method Attribute
+    </title>
+</head>
+  
+<body style="text-align:center;"> 
+  
+    <h1 style="color:green;">GeeksforGeeks</h1> 
+  
+    <h3> HTML <form>Its  Me </h3> 
+</body> 
+  
+</html> 
